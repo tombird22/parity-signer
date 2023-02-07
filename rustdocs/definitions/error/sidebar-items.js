4ccdcc0d8b4d44c530400d8878cd44110c1b8381
montekki <montekki@users.noreply.github.com>
2023-02-07 05:50:10 +0000

    deploy: 34f5e5e81f8e23b70889dd258fc885583b8c8d4b

---
+++ rustdocs/definitions/error/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":[["Error",""],["MetadataError","Intrinsic problems of the metadata making it unsuitable for Signer use"],["TransferContent","Error decoding transfer content"]],"type":[["Result",""]]};
